e7bec524659b8f7713d88809d26fcb08d7f25f23
Tilak S Naik <tilakchinnu96@gmail.com>
2017-07-22 12:35:22 +0530

    Login

--- app/src/main/java/in/goflo/baymax/MenuActivity.java
+++ app/src/main/java/in/goflo/baymax/MenuActivity.java
@@ -5,16 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_menu);
     }
 
-    public void onClick (View view) {
-        startActivity(new Intent(LoginActivity.this,MenuActivity.class));
+    public void imageCapture(View v){
+        startActivity(new Intent(MenuActivity.this,UploadPhotoActivity.class));
     }
-
 }
--- app/src/main/java/in/goflo/baymax/UploadPhotoActivity.java
+++ app/src/main/java/in/goflo/baymax/UploadPhotoActivity.java
@@ -18,13 +18,31 @@ package in.goflo.baymax;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UploadPhotoActivity extends AppCompatActivity {
 
@@ -55,6 +73,7 @@ public class UploadPhotoActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
+            saveProfileAccount(imageBitmap);
         }
     }
 
@@ -64,5 +83,104 @@ public class UploadPhotoActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    private void saveProfileAccount(final Bitmap bitmap) {
+        // loading or check internet connection or something...
+        // ... then
+        String url = "http://goflo.in:5000/upload";
+        VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, url, new Response.Listener<NetworkResponse>() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+                String resultResponse = new String(response.data);
+                Log.v("upload",resultResponse);
+                try {
+                    JSONObject result = new JSONObject(resultResponse);
+                    final String classifiedType = result.getString("type");
+                    final String status = result.getString("status");
+
+                    if (status.equals("200")) {
+                        // tell everybody you have succed upload image and post strings
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(UploadPhotoActivity.this,"Type = "+classifiedType,Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } else {
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(UploadPhotoActivity.this,"upload failed",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                String errorMessage = "Unknown error";
+                if (networkResponse == null) {
+                    if (error.getClass().equals(TimeoutError.class)) {
+                        errorMessage = "Request timeout";
+                    } else if (error.getClass().equals(NoConnectionError.class)) {
+                        errorMessage = "Failed to connect server";
+                    }
+                } else {
+                    String result = new String(networkResponse.data);
+                    try {
+                        JSONObject response = new JSONObject(result);
+                        String status = response.getString("status");
+                        String message = response.getString("message");
+
+                        Log.e("Error Status", status);
+                        Log.e("Error Message", message);
+
+                        if (networkResponse.statusCode == 404) {
+                            errorMessage = "Resource not found";
+                        } else if (networkResponse.statusCode == 401) {
+                            errorMessage = message+" Please login again";
+                        } else if (networkResponse.statusCode == 400) {
+                            errorMessage = message+ " Check your inputs";
+                        } else if (networkResponse.statusCode == 500) {
+                            errorMessage = message+" Something is getting wrong";
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.i("Error", errorMessage);
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                /*params.put("api_token", "gh659gjhvdyudo973823tt9gvjf7i6ric75r76");
+                params.put("name", mNameInput.getText().toString());
+                params.put("location", mLocationInput.getText().toString());
+                params.put("about", mAvatarInput.getText().toString());
+                params.put("contact", mContactInput.getText().toString());*/
+                return params;
+            }
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                // file name could found file base or direct access from real path
+                // for now just get bitmap data from ImageView
+                Drawable d = new BitmapDrawable(getResources(), bitmap);
+                params.put("image", new DataPart("image.jpg", AppHelper.getFileDataFromDrawable(getBaseContext(), d), "image/jpeg"));
+                //params.put("cover", new DataPart("file_cover.jpg", AppHelper.getFileDataFromDrawable(getBaseContext(), mCoverImage.getDrawable()), "image/jpeg"));
+
+                return params;
+            }
+        };
+
+        VolleySingleton.getInstance(getBaseContext()).addToRequestQueue(multipartRequest);
     }
 }
